cf7fa510e3a503a87a84f95357e3d7b798f5162d
Buñay <ba0100069u@baustro.fin.ec>
2022-08-22 12:30:55 -0500

    new change in air console web

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "New change is coming soon console! ";
+print "New change is coming soon console!\n ";
+print "New change in Air!\n";
 ?>
